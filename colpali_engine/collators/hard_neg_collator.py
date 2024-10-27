379195d2bbb5ca23314d052aa55684543d37ddc4
Vahid Majdinasab <a.majdinasab@hotmail.com>
2024-10-27 08:06:52 -0400

    feat(colpali_engine/collators): added `TAIHardNegCollator`.
    The default datasets that colpali uses, have a structed directory schema, with the images existing in different directories. We do not store the images in a separate directory. So we need our own collator to read the data from the training data JSONL to read the images and convert them to image objects that can be used for fine-tuning

--- colpali_engine/collators/hard_neg_collator.py
+++ colpali_engine/collators/hard_neg_collator.py
@@ -1,6 +1,8 @@
 from random import randint
 from typing import Any, Dict, List, Optional, cast
-
+from PIL import Image
+from io import BytesIO
+import base64
 from datasets import Dataset
 
 from colpali_engine.collators.visual_retriever_collator import VisualRetrieverCollator
@@ -41,3 +43,44 @@ class HardNegCollator(VisualRetrieverCollator):
             examples += [{"image": pos_image, "query": pos_query, "neg_image": neg_image}]
 
         return super().__call__(examples)
+
+class TAIHardNegCollator(VisualRetrieverCollator):
+    def __init__(
+        self,
+        processor: BaseVisualRetrieverProcessor,
+        max_length: int = 2048,
+        add_suffix: bool = True,
+        image_dataset: Optional[Dataset] = None,
+    ):
+        super().__init__(
+            processor=processor,
+            max_length=max_length,
+            add_suffix=add_suffix,
+        )
+        if image_dataset is None:
+            raise ValueError("`image_dataset` must be provided")
+        self.image_dataset = cast(Dataset, image_dataset)
+
+    def get_image_from_image_dataset(self, image_idx):
+        return self.image_dataset[int(image_idx)]["image"]
+
+    def __call__(self, examples: List[Dict[str, Any]]) -> Dict[str, Any]:
+        tmp_examples = examples
+        examples = []
+
+        for example in tmp_examples:
+            pos_image = Image.open(BytesIO(base64.b64decode(example["gold_index"])))
+
+            # pos_image = example["gold_index"]
+            pos_query = example["query"]
+
+            # Randomly sample a negative image amongst the top 10
+            neg_image = Image.open(BytesIO(base64.b64decode(example["negs"][0])))
+            # neg_image = example["negs"][0]
+
+            examples += [
+                {"image": pos_image, "query": pos_query, "neg_image": neg_image}
+            ]
+
+        return super().__call__(examples)
+
